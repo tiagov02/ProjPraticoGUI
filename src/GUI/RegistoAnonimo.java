6cf21873dbaf8fff28b8dbbe25db32b296f7357a
Tiago Viana <tiagoviana.1003@gmail.com>
2021-12-30 18:03:22 +0000

    [30/12-18h03]

--- src/GUI/RegistoAnonimo.java
+++ src/GUI/RegistoAnonimo.java
@@ -2,6 +2,8 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RegistoAnonimo extends JFrame{
     private JButton clienteButton;
@@ -9,7 +11,7 @@ public class RegistoAnonimo extends JFrame{
     private JPanel RegAnonimoPanel;
     private JFrame RegAnonimo;
 
-    public RegistoAnonimo(){
+    public RegistoAnonimo() {
         RegAnonimo = new JFrame("Login Utilizador");
         RegAnonimo.setDefaultCloseOperation(EXIT_ON_CLOSE);
         RegAnonimo.setPreferredSize(new Dimension(300, 300));
@@ -18,7 +20,27 @@ public class RegistoAnonimo extends JFrame{
         RegAnonimo.add(RegAnonimoPanel);
         RegAnonimo.pack();
         RegAnonimo.setVisible(true);
+        clickCliente();
+        clickDonoEmpresa();
+    }
 
-        //CRiar listener
+    public void clickCliente(){
+        clienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RegAnonimo.setVisible(false);
+                new RegistoCliente();
+            }
+        });
+    }
+
+    public void clickDonoEmpresa(){
+        DonoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RegAnonimo.setVisible(false);
+                new RegistoDonoEmpresa();
+            }
+        });
     }
 }
