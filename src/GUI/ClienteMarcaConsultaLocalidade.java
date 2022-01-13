324f5905466b9df51b8d6c5d8ff6c79b9cbdf07f
Tiago Viana <tiagoviana.1003@gmail.com>
2022-01-13 00:16:00 +0000

    zoom

--- src/GUI/ClienteMarcaConsultaLocalidade.java
+++ src/GUI/ClienteMarcaConsultaLocalidade.java
@@ -1,11 +1,14 @@
 package GUI;
 
-import MetodosLogicos.AnonimoMetodos;
 import Repositorio.Repositorio;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Entidades.*;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
@@ -13,8 +16,11 @@ public class ClienteMarcaConsultaLocalidade {
     private JPanel panel;
     private JScrollPane scroll;
     private JTable table1;
+    private JButton okButton;
+    private JTextField tb_nif;
 
     public ClienteMarcaConsultaLocalidade(JFrame frame){
+        Repositorio r=Repositorio.getInstance();
         frame = new JFrame("Registo de Clientes");
 
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -28,5 +34,26 @@ public class ClienteMarcaConsultaLocalidade {
         model.addColumn("Telefone da empresa");
         model.addColumn("Localidade");
 
+        for(Empresa e:r.getEmpresasLocalidade().keySet()){
+            if(r.getEmpresasLocalidade().get(e).equals(r.getCurrentUser().getLocalidade())){
+                model.addRow(new Object[] {e.getNomeEmpresa(),e.getNif(),e.getTelefone(),e.getLocalidade()});
+            }
+        }
+        clickOK();
+    }
+
+    public void clickOK(){
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int nif=0;
+                try{
+                    nif=Integer.parseInt(tb_nif.getText());
+                }
+                catch(NumberFormatException ex){
+                    JOptionPane.showMessageDialog(null,"Não pode introduzir letras no nif");
+                }
+            }
+        });
     }
 }
