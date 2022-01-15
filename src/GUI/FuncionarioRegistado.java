19ea58743308bb924839d1bc089b7344621ba4bd
Tiago Viana <tiagoviana.1003@gmail.com>
2022-01-15 02:28:52 +0000

    [15/01-02h12]

--- src/GUI/FuncionarioRegistado.java
+++ src/GUI/FuncionarioRegistado.java
@@ -1,4 +1,17 @@
 package GUI;
 
+import javax.swing.*;
+
 public class FuncionarioRegistado {
+    private JButton btn_listConsultas;
+    private JButton btn_alterarMarcacao;
+    private JButton btn_DadosDiag;
+    private JButton btn_criaConsulta;
+    private JPanel panel;
+
+    public FuncionarioRegistado(JFrame frame){
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
