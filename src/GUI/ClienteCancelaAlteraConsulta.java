03862f2b450238e3f58665ef283cb9109788d988
Tiago Viana <tiagoviana.1003@gmail.com>
2022-01-17 21:35:34 +0000

    17/01

--- src/GUI/ClienteCancelaAlteraConsulta.java
+++ src/GUI/ClienteCancelaAlteraConsulta.java
@@ -1,4 +1,17 @@
 package GUI;
 
+import javax.swing.*;
+
 public class ClienteCancelaAlteraConsulta {
+    private JPanel panel1;
+    private JTable table1;
+    private JButton buttonback;
+    private JButton Alterar;
+    private JComboBox cb_cliente;
+    private JButton cancelarConsultaButton;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+    private JSpinner spinner4;
+    private JSpinner spinner5;
 }
