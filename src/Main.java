e30b880aa05abac7a051f9f1c85739e562f99974
Tiago Viana <tiagoviana.1003@gmail.com>
2022-01-18 09:41:57 +0000

    redimensionamento janelas

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
 
         JFrame frame = new JFrame("PetBeauty- Gestão de Marcações e Consultas de Animais de Estimação");
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setPreferredSize(new Dimension(700, 700));
         frame.setResizable(true);
         new Login(frame);
     }
